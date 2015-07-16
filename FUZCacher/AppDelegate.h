c9a620cab834e8f10e484baff319a713c74ee27c
fuzza <alex.fuzza@gmail.com>
2015-07-16 22:14:14 +0300

    Base project setup

---
+++ FUZCacher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FUZCacher
+//
+//  Created by fuzza on 7/16/15.
+//  Copyright (c) 2015 fuzza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FUZCacher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FUZCacher
+//
+//  Created by fuzza on 7/16/15.
+//  Copyright (c) 2015 fuzza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
